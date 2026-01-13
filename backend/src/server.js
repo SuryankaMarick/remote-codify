b85d738d19c132b3dab6052d09ad90f3b8b1fbd9
Suryanka Marick <surya9088907993@gmail.com>
2026-01-13 12:22:44 +0530

    fixing error

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,7 @@ const __dirname = path.resolve();
 
 // middlewares
 app.use(express.json());
+app.use(clerkMiddleware());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use('/api/inngest', serve({ client: inngest, functions }));
 
